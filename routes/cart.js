94c4caf61cbe14efb677854d7048574fdecfec63
Destinycod <65719901+Destinycod@users.noreply.github.com>
2022-11-14 18:31:14 -0300

    Update routes files

--- routes/cart.js
+++ routes/cart.js
@@ -1,57 +1,57 @@
 const router = require("express").Router();
 const Cart = require("../models/Cart");
-const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
+//const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
 
 //CREATE
-router.post("/", verifyToken, async (req,res)=>{
+router.post("/", async (req,res)=>{//, verifyToken
 
     const newCart = new Cart(req.body);
     try{
         const savedCart = await newCart.save();
-        res.status(201).send(savedCart);
+        return res.status(201).send(savedCart);
     }
     catch(error){
-        res.status(500).json(error);
+        return res.status(500).json(error);
     }
 });
 
 //UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
-        res.status(200).json(updatedCart);
-    }catch(error){
-        res.status(500).json(error);
+router.put("/:id", async (req,res)=>{//, verifyTokenAndAuthorization
+    if(req.params.id.match(/^[0-9a-fA-F]{24}$/)){
+        try{
+            const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+            return res.status(200).json(updatedCart);
+        }catch(error){
+            return res.status(500).json(error);
+        }
+    }else{
+        return res.status(404).json("Not Found");
     }
 });
 
-//DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        await Cart.findByIdAndDelete(req.body.id);
-        res.status(204).json("Cart has been deleted");
-    }catch(error){
-        res.status(500).json(error);        
-    }
-});
-
-//GET USER CART BY ID
-router.get("/:userId", verifyTokenAndAuthorization, async (req,res)=>{
-    try{
-        const cart = await Cart.findOne({userId: req.params.userId});
-        res.status(200).json(cart);
-    }catch(error){
-        res.status(500).json(error);        
-    }
-});
-
-//GET ALL CARTS OF ALL USERS
-router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+//GET ALL CARTS
+router.get("/", async (req,res)=>{//
     try{
         const carts = await Cart.find();
-        res.status(200).json(carts);
+        return res.status(200).json(carts);
     }catch(error){
-        res.status(500).json(error);        
+        return res.status(500).json(error);        
+    }
+});
+
+//GET BY ID?
+
+//DELETE
+router.delete("/:id", async (req,res)=>{//, verifyTokenAndAuthorization
+    if(req.params.id.match(/^[0-9a-fA-F]{24}$/)){
+        try{
+            await Cart.findByIdAndDelete(req.params.id);
+            return res.status(204).json("Cart has been deleted");
+        }catch(error){
+            return res.status(500).json(error);        
+        }
+    }else{
+        return res.status(404).json("Not Found");
     }
 });
 
